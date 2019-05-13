ebf71c29250363b28b16f71ece269583c8b56e33
eFGie <filip.grodzki@gmail.com>
2019-05-13 17:28:05 +0200

    Calc test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Test");
+        Calculator calc = new Calculator();
+
+        int addResult= calc.add(5,16);
+        if (addResult == 21){
+            System.out.println("add test OK");
+        } else {
+            System.out.println("add test Error!");
+        }
+
+        int substractResult= calc.substract(5,16);
+        if (substractResult == -11){
+            System.out.println("substract test OK");
+        } else {
+            System.out.println("substract test Error!");
+        }
+
     }
 }
