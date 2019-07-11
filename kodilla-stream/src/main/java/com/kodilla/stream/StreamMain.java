c0270a4714facffd42a7f77c2ad0637b620d4f47
eFGie <filip.grodzki@gmail.com>
2019-07-11 22:27:16 +0200

    Commit

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream;
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -19,12 +20,15 @@ public class StreamMain {
 //        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
 //        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 //
+//
+//      PoemBeautifier poemBeautifier = new PoemBeautifier();
+//      poemBeautifier.beautify("blabla",textToBeutify -> "ABC_" + textToBeutify + "_ABC");
+//      poemBeautifier.beautify("blabla",textToBeutify ->textToBeutify.toUpperCase());
+//      poemBeautifier.beautify("blabla",textToBeutify -> textToBeutify.replace("a","BLA"));
+//      poemBeautifier.beautify("     bla_bla         ",textToBeutify ->textToBeutify.trim() );
+//
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("blabla",textToBeutify -> "ABC_" + textToBeutify + "_ABC");
-        poemBeautifier.beautify("blabla",textToBeutify ->textToBeutify.toUpperCase());
-        poemBeautifier.beautify("blabla",textToBeutify -> textToBeutify.replace("a","BLA"));
-        poemBeautifier.beautify("     bla_bla         ",textToBeutify ->textToBeutify.trim() );
-
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
