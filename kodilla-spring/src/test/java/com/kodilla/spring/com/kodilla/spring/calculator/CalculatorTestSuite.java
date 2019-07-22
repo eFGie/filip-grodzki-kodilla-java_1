19bd200e25912d8b5438c79feeeb67e2e0b2db96
eFGie <filip.grodzki@gmail.com>
2019-07-22 23:06:35 +0200

    Commit

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,26 @@
+package com.kodilla.spring.com.kodilla.spring.calculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+    @Test
+    public void testCalculations() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        calculator.add(1,4);
+        calculator.sub(1,4);
+        calculator.mul(1,4);
+        calculator.div(1,4);
+    }
+
+}
